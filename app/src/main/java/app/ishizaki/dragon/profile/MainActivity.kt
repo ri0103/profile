3a9b173d5ff29d8d737a8503796ea3f52bbf4920
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-20 22:35:39 +0900

    profile1

---
+++ app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
@@ -0,0 +1,16 @@
+package app.ishizaki.dragon.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.ishizaki.dragon.profile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+    }
+}
