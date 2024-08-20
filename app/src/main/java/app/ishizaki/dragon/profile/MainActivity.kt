242d11b2acb4cdc6f11b60ff650e4a467345f082
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-20 23:12:26 +0900

    profile3

--- app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
+++ app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
@@ -14,13 +14,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
-        binding.profileImage.isVisible = false
-        binding.profileCommentText.isVisible = false
+//        binding.profileImage.isVisible = false
+//        binding.profileCommentText.isVisible = false
 
         binding.showProfileButton.setOnClickListener {
-            binding.profileImage.isVisible = true
-            binding.profileCommentText.isVisible = true
+            binding.profileImage.setImageResource(R.drawable.randy_image)
+            binding.profileLabelText.text = "名前"
+            binding.profileCommentText.text = "Androidメンターのランディだよ"
+        }
 
+        binding.showSportButton.setOnClickListener {
+            binding.profileImage.setImageResource(R.drawable.baseball_image)
+            binding.profileLabelText.text = "スポーツ"
+            binding.profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+        }
+
+        binding.showFoodButton.setOnClickListener {
+            binding.profileImage.setImageResource(R.drawable.donut_image)
+            binding.profileLabelText.text = "好きな食べ物"
+            binding.profileCommentText.text = "キャンディやドーナツが大好き"
+        }
+
+        binding.showHobbyButton.setOnClickListener {
+            binding.profileImage.setImageResource(R.drawable.gadget_image)
+            binding.profileLabelText.text = "趣味"
+            binding.profileCommentText.text = "ガジェットを集めて動かすこと"
         }
     }
 }
