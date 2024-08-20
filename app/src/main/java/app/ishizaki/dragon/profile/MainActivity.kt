39f93d73cfcb37037e09d97852f03f8458804249
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-20 23:18:19 +0900

    profile4

--- app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
+++ app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
@@ -1,5 +1,7 @@
 package app.ishizaki.dragon.profile
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,24 +23,46 @@ class MainActivity : AppCompatActivity() {
             binding.profileImage.setImageResource(R.drawable.randy_image)
             binding.profileLabelText.text = "名前"
             binding.profileCommentText.text = "Androidメンターのランディだよ"
+            binding.showProfileButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(0, 180 ,220))
+            binding.showSportButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showFoodButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showHobbyButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
         }
 
         binding.showSportButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.baseball_image)
             binding.profileLabelText.text = "スポーツ"
             binding.profileCommentText.text = "野球観戦が好きで、スタジアムに通っている"
+
+            binding.showProfileButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showSportButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(220, 0 ,100))
+            binding.showFoodButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showHobbyButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+
         }
 
         binding.showFoodButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.donut_image)
             binding.profileLabelText.text = "好きな食べ物"
             binding.profileCommentText.text = "キャンディやドーナツが大好き"
+
+            binding.showProfileButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showSportButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showFoodButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(0, 180 ,120))
+            binding.showHobbyButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+
         }
 
         binding.showHobbyButton.setOnClickListener {
             binding.profileImage.setImageResource(R.drawable.gadget_image)
             binding.profileLabelText.text = "趣味"
             binding.profileCommentText.text = "ガジェットを集めて動かすこと"
+
+            binding.showProfileButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showSportButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showFoodButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(150, 150 ,150))
+            binding.showHobbyButton.backgroundTintList = ColorStateList.valueOf(Color.rgb(220, 100 ,0))
+
         }
     }
 }
