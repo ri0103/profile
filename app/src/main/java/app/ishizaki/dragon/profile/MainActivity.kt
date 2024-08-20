18c12fbf32c2df4fd6eef4834775c9c2017b3872
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-20 22:53:12 +0900

    profile2

--- app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
+++ app/src/main/java/app/ishizaki/dragon/profile/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import app.ishizaki.dragon.profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        binding.profileImage.isVisible = false
+        binding.profileCommentText.isVisible = false
+
+        binding.showProfileButton.setOnClickListener {
+            binding.profileImage.isVisible = true
+            binding.profileCommentText.isVisible = true
+
+        }
     }
 }
